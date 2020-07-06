6fc27132ca887fa2d591246e065c238a76e9b3be
evalton <evalton.nunes@gmail.com>
2020-07-06 06:31:36 -0300

    ajustes service

--- src/main/java/br/backend/api/service/UserService.java
+++ src/main/java/br/backend/api/service/UserService.java
@@ -1,21 +1,17 @@
 package br.backend.api.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.backend.api.entity.Livro;
-import br.backend.api.entity.Telefone;
 import br.backend.api.entity.User;
 import br.backend.api.entity.dto.UserDTO;
 import br.backend.api.entity.dto.UserMapper;
 import br.backend.api.entity.dto.UserUpdateDTO;
 import br.backend.api.entity.dto.telefone.TelefoneMapper;
 import br.backend.api.exception.ExceptionErros;
-import br.backend.api.repository.TelefoneRepository;
 import br.backend.api.repository.UserRepository;
 import br.backend.api.utils.SenhaUtil;
 
@@ -43,7 +39,7 @@ public class UserService {
 	
 	public List<UserDTO> listar(){
 		List<User> usuarios = repo.findAll();
-		return userMapper.toDtoList(usuarios);
+		return  userMapper.toDtoList(usuarios);
 	}
 	
 	public User buscarPorEmail(String email) {
@@ -57,7 +53,7 @@ public class UserService {
 				"Usuario não encontrada Id:"+id+",: "+ User.class.getName()));
 	}
 	
-	public UserDTO atualizar(UserUpdateDTO dto, Long id) throws ExceptionErros {
+	public  UserDTO atualizar(UserUpdateDTO dto, Long id) throws ExceptionErros {
 		User user = buscar(id);		
 		userMapper.updateModel(dto, user);
 		setFilhos(user);
@@ -72,20 +68,8 @@ public class UserService {
 				"Usuario não encontrada Id:"+id+", tipo: "+ User.class.getName()));
 	}
 	
-	private void setFilhos(User user) throws ExceptionErros {
-		List<Telefone> telefones = new ArrayList<>();
-		List<Livro> livros = new ArrayList<>();			
+	private void setFilhos(User user) throws ExceptionErros {	
 		user.getTelefones().forEach(t -> {
-//			if(t.getId()!=null) {
-//				try {
-//					Telefone telefone = telService.buscarPorId(t.getId());
-//					telefones.add(telefone);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}			
-//			}else {
-//				
-//			}
 			t.setUser(user);
 		});
 		
